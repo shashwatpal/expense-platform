ec2a30b47cbb61b2a89bb9702fb488ea0ddc580a
Shashwat Pal <palshashwat6930@gmail.com>
2026-06-13 15:48:23 +0530

    First commit

---
+++ src/main/java/com/example/expense_platform/ExpensePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.expense_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpensePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpensePlatformApplication.class, args);
+	}
+
+}
