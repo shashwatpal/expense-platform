7bad91807a3ba3d69dc5f3aeb7549e42042b5f4a
Shashwat Pal <palshashwat6930@gmail.com>
2026-06-13 17:12:40 +0530

    Adding basic dependencies and config

--- src/test/java/com/example/expense_platform/ExpensePlatformApplicationTests.java
+++ src/test/java/com/example/expense_platform/ExpensePlatformApplicationTests.java
@@ -1,13 +1,1 @@
-package com.example.expense_platform;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ExpensePlatformApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
